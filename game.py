d8af4aa21dc5327da67e22f4c00a0ff7267bcfc3
Artem Kozin <akozin@car10s1.42barcelona.com>
2026-07-21 15:35:16 +0200

    basic game field setup. cell types enum setup

--- game.py
+++ game.py
@@ -1,2 +1,23 @@
+from utils.cell_types import Cell
+import math
+
+
 class Game:
-    """Describes the game field. Can act on it, can return various info"""
+    """Describes the game field. Can act on it, can return general info"""
+    dimension = 10
+    size = dimension + 2
+    def __init__(self):
+        #   x - >
+        # y  0  1  2  3  4  5  6  7  8  9 10 11
+        # | 12 13 14 15 16 17 18 19 20 21 22 23
+        # v
+        #
+        # so, to get x: % 12
+        # to get y: / 12
+        self.board = [
+            Cell.EMPTY if (
+                (i % self.size != 0)
+                and (i % self.size != self.size - 1)
+                and (i > self.size)
+                and (i < self.size*(self.size - 1)))
+            else Cell.WALL for i in range(12*12)]
---
+++ utils/cell_types.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class Cell(Enum):
+    EMPTY = 0
+    WALL = 1
+    HEAD = 2
+    BODY = 3
+    GREEN_APPLE = 4
+    RED_APPLE = 5
